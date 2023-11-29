ab3366b866165770e213aa6e234663e91707896f
efratMIL <144050310+efratMIL@users.noreply.github.com>
2023-11-29 20:35:49 +0200

    more bugs

--- src/Componnents/GameBoard.jsx
+++ src/Componnents/GameBoard.jsx
@@ -1,18 +1,19 @@
 import React, { useState } from 'react';
 
 function GameBoard(props) {
-  console.log(props.game);
-
   const [number, setNumber] = useState(Math.floor(Math.random() * 100));
-  const [countSteps, setcountSteps] = useState(0);
+  const [countSteps, setCountSteps] = useState(0);
+
   function handleOperations(operation) {
     setNumber((prevNumber) => eval(`${prevNumber} ${operation}`));
-    setcountSteps((prevCount) => prevCount += 1);
+    setCountSteps((prevCount) => prevCount + 1);
   }
+
   return (
     <div>
-      <span>{props.game.player.userName}</span>
+      <span>{props.game.player.username}</span>
       <p>Random Number: {number}</p>
+      <p>{props.game.player.disable}</p>
       <button onClick={() => handleOperations('/ 2')}>/2</button>
       <button onClick={() => handleOperations('* 2')}>*2</button>
       <button onClick={() => handleOperations('+ 1')}>+1</button>
--- src/Componnents/GameBoardsDisplay.jsx
+++ src/Componnents/GameBoardsDisplay.jsx
@@ -1,18 +1,22 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import GameBoard from './GameBoard';
+
 function GameBoardsDisplay(props) {
-    function getGames() {
-        return props.currentPlayers.map((player, index) => ({ player: player, disable: index===0?false:true }));
-    }
-    const [games, setGames] = useState(getGames());
+    const [games, setGames] = useState(props.currentPlayers.map((player, index) => ({ player: player, disable: index === 0 ? false : true })));
+
     console.log(games);
+
+    if (games.length !== props.currentPlayers.length) {
+        setGames(props.currentPlayers.map((player, index) => ({ player: player, disable: index === 0 ? false : true })));
+    }
 
     return (
         <div>
-            {games.map((game, index) =>
+            {games.map((game, index) => (
                 <GameBoard key={index} game={game} />
-            )}
+            ))}
         </div>
-    )
+    );
 }
-export default GameBoardsDisplay
+
+export default GameBoardsDisplay;
